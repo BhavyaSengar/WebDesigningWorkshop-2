b0e1aa8ba85ca154769a414ba070f26b4743430e
Bhavya Sengar <bhavyahts@gmail.com>
2026-03-12 12:09:33 +0530

    Sixth

---
+++ Patterns/6-Inverted_Triangle.js
@@ -0,0 +1,15 @@
+let n = 5;
+
+for (let i = n; i >= 1; i--) {
+    let row = "";
+
+    for (let j = 1; j <= n - i; j++) {
+        row += " ";
+    }
+
+    for (let k = 1; k <= i; k++) {
+        row += "* ";
+    }
+
+    console.log(row);
+}
