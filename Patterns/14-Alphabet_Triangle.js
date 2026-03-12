4ed0cd140b0c1d9feba02d29eddbff0ecf23c0f4
Bhavya Sengar <bhavyahts@gmail.com>
2026-03-12 12:15:10 +0530

    Forteenth

---
+++ Patterns/14-Alphabet_Triangle.js
@@ -0,0 +1,12 @@
+let n = 5;
+
+for(let i = 0; i < n; i++){
+    let row = "";
+    let char = String.fromCharCode(65 + i);
+
+    for(let j = 0; j <= i; j++){
+        row += char + " ";
+    }
+
+    console.log(row);
+}
