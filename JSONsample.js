d77c27368fb5edad08c47f9871d225d88e89fb6a
Bhavya Sengar <bhavyahts@gmail.com>
2026-03-19 11:12:59 +0530

    second

--- JSONsample.js
+++ JSONsample.js
@@ -1,3 +1,14 @@
-let jsonString=`{"name":"Bhavya","age":25}`;
-let obj=JSON.parse(jsonString);
-console.log(obj);
+// let jsonString=`{"name":"Bhavya","age":25}`;
+// let obj=JSON.parse(jsonString);
+// console.log(obj);
+
+
+let student={
+    name:"Rahul",
+    age:21,
+    course:"CSE"
+};
+
+for(let key in student){
+    console.log(key+" : "+student[key]);
+}
