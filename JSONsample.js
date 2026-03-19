f5be428e0f33e4c21bf9cb65f1c5c851a16e8dc9
Bhavya Sengar <bhavyahts@gmail.com>
2026-03-19 11:16:24 +0530

    Third

--- JSONsample.js
+++ JSONsample.js
@@ -3,12 +3,17 @@
 // console.log(obj);
 
 
-let student={
-    name:"Rahul",
-    age:21,
-    course:"CSE"
-};
+// let student={
+//     name:"Rahul",
+//     age:21,
+//     course:"CSE"
+// };
 
-for(let key in student){
-    console.log(key+" : "+student[key]);
+// for(let key in student){
+//     console.log(key+" : "+student[key]);
+// }
+
+let number = [10,20,30,40];
+for(let value of number){
+    console.log(value);
 }
